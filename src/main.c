5bf7bea55612a92ce39b7a244e72c61fbf41809d
Samuel Tavares da Silva <ls30759@salleurl.edu>
2018-03-16 11:21:29 +0100

    Practice ready
    We need do some refactor and comment the code.

--- src/main.c
+++ src/main.c
@@ -14,16 +14,19 @@
 #include "stm32f4xx_tim.h"
 #include "stm32f4xx_gpio.h"
 #include "tm_stm32f4_rng.h"
+
+#define MAX_PERIODE 28800
 /* Include my libraries here */
 
 
 
 /* Variables globales */
-long timer_value;
+int timer_value;
+int tm4_periode;
 /*
  * Function for scale a number to the desired range.
  */
-long map(int rand, int min, int max)
+int map(int rand, int min, int max)
 {
   return (rand % (max + 1 - min)) + min;
 }
@@ -60,6 +63,7 @@ void TM_LEDS_PWM_Init(void) {
     GPIO_Init(GPIOD, &GPIO_InitStruct);
 }
 
+
 void initLed3(GPIO_InitTypeDef gpio){
 		//RCC -> Reset and Clock Control
 		// All GPIO's are on AHB1 bus.
@@ -75,7 +79,8 @@ void initLed3(GPIO_InitTypeDef gpio){
 			GPIO_Speed: Choose pin speed
 		 *
 		 */
-		gpio.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_1;
+
+		gpio.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_5;
 		gpio.GPIO_Mode = GPIO_Mode_OUT;
 		//una vez digamos que hemos indicado que "usaremos" en las linias anteriores
 		//ahora se lo indicamos por asi decirlo, a la placa
@@ -101,29 +106,96 @@ void initButton(GPIO_InitTypeDef gpio){
 
 }
 
-/**
- *
- * 500 periodes i 13 presc.
- * 64p i 10presc--> Periode de 50us
- *
- */
+/* Configure pins to be interrupts */
+void Configure_PD0(void) {
+	/* Set variables used */
+	GPIO_InitTypeDef GPIO_InitStruct;
+	EXTI_InitTypeDef EXTI_InitStruct;
+	NVIC_InitTypeDef NVIC_InitStruct;
+
+	/* Enable clock for GPIOD */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	/* Enable clock for SYSCFG */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+	/* Set pin as input */
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+	/* Tell system that you will use PD0 for EXTI_Line0 */
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource0);
+
+	/* PD0 is connected to EXTI_Line0 */
+	EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+	/* Enable interrupt */
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	/* Interrupt mode */
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	/* Triggers on rising and falling edge */
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+	/* Add to EXTI */
+	EXTI_Init(&EXTI_InitStruct);
+
+	/* Add IRQ vector to NVIC */
+	/* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+	/* Set priority */
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+	/* Set sub priority */
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+	/* Enable interrupt */
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	/* Add to NVIC */
+	NVIC_Init(&NVIC_InitStruct);
+}
+
+/* Set interrupt handlers */
+/* Handle PD0 interrupt */
+void EXTI0_IRQHandler(void) {
+	/* Make sure that interrupt flag is set */
+	if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+		EXTI_ClearITPendingBit(EXTI_Line0);
+		GPIO_SetBits(GPIOG, GPIO_Pin_5);
+		if(tm4_periode > MAX_PERIODE) {
+			GPIO_SetBits(GPIOG, GPIO_Pin_14);
+			GPIO_ResetBits(GPIOG, GPIO_Pin_13);
+		}else{
+			GPIO_SetBits(GPIOG, GPIO_Pin_13);
+		}
+
+	}
+}
+
 void TIM2_INT_Init()
 {
-    // Enable clock for TIM2
+	// Nested vectored interrupt settings
+	// TIM2 interrupt is most important (PreemptionPriority and
+	// SubPriority = 0)
+	NVIC_InitTypeDef NVIC_InitStruct;
+	NVIC_InitStruct.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStruct);
+
+
+	// Enable clock for TIM2
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
-    // TIM2 initialization overflow every 500ms
-    // TIM2 by default has clock of 84MHz
-    // Here, we must set value of prescaler and period,
-    // so update event is 0.5Hz or 500ms
-    // Update Event (Hz) = timer_clock / ((TIM_Prescaler + 1) *
-    // (TIM_Period + 1))
-    // Update Event (Hz) = 84MHz / ((4199 + 1) * (9999 + 1)) = 0.5 Hz
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
-    TIM_TimeBaseInitStruct.TIM_Prescaler = 9;
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 0;
     TIM_TimeBaseInitStruct.TIM_Period = 287;
     TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+
+
+
+    timer_value = 0;
+
 
     // TIM2 initialize
     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
@@ -131,27 +203,85 @@ void TIM2_INT_Init()
     TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
     // Start TIM2
     TIM_Cmd(TIM2, ENABLE);
-    timer_value = 0;
-
-    // Nested vectored interrupt settings
-    // TIM2 interrupt is most important (PreemptionPriority and
-    // SubPriority = 0)
-    NVIC_InitTypeDef NVIC_InitStruct;
-    NVIC_InitStruct.NVIC_IRQChannel = TIM2_IRQn;
-    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStruct);
 }
 
 void TIM2_IRQHandler()
 {
     // Checks whether the TIM2 interrupt has occurred or not
-    if (TIM_GetITStatus(TIM2, TIM_IT_Update))
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update)!=RESET)
     {
     	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    	//GPIO_ToggleBits(GPIOG, GPIO_Pin_2);
     	timer_value++;
     }
+
+
+}
+
+
+void TM_PWM_Init(int dutycycle) {
+
+	TIM_OCInitTypeDef TIM_OCStruct;
+
+
+	TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM2;
+	TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
+	TIM_OCStruct.TIM_Pulse = dutycycle;
+	TIM_OC1Init(TIM4, &TIM_OCStruct);
+	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+}
+
+
+
+void TIM4_INT_Init(int periode)
+{
+    // Enable clock for TIM
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 9;
+    TIM_TimeBaseInitStruct.TIM_Period = periode;
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+
+    // TIM2 initialize
+    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseInitStruct);
+    // Enable TIM2 interrupt
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    // Start TIM4
+    TIM_Cmd(TIM4, ENABLE);
+    //timer_value = 0;
+
+    NVIC_InitTypeDef NVIC_InitStruct;
+    NVIC_InitStruct.NVIC_IRQChannel = TIM4_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+
+    timer_value = 0;
+    //TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+
+
+    NVIC_Init(&NVIC_InitStruct);
+    GPIO_ResetBits(GPIOG, GPIO_Pin_14);
+}
+
+void TIM4_IRQHandler()
+{
+    // Checks whether the TIM2 interrupt has occurred or not
+    if (TIM_GetITStatus(TIM4, TIM_IT_Update)!=RESET)
+    {
+    	TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+
+    	//GPIO_SetBits(GPIOG, GPIO_Pin_2);
+
+
+
+    	//timer_value++;
+    }
+
+
 }
 
 void TM_TIMER_PWM_Init(int period) {
@@ -163,7 +293,7 @@ void TM_TIMER_PWM_Init(int period) {
 	TIM_BaseStruct.TIM_Prescaler = 9;
 	/* Count up */
     TIM_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_BaseStruct.TIM_Period = 287*period; /* 10kHz PWM */
+    TIM_BaseStruct.TIM_Period = 8399; /* 10kHz PWM */
     TIM_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_BaseStruct.TIM_RepetitionCounter = 0;
 	/* Initialize TIM4 */
@@ -172,91 +302,63 @@ void TM_TIMER_PWM_Init(int period) {
     TIM_Cmd(TIM4, ENABLE);
 }
 
-void TM_PWM_Init(int periode) {
-	int pulse_length = (((periode*287)+ 1)* 50) / 100-1;
-	TIM_OCInitTypeDef TIM_OCStruct;
 
-	/* Common settings */
-
-	/* PWM mode 2 = Clear on compare match */
-	/* PWM mode 1 = Set on compare match */
-	TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM2;
-	TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
-
-/*
-	To get proper duty cycle, you have simple equation
-
-	pulse_length = ((TIM_Period + 1) * DutyCycle) / 100 - 1
-
-
-
-	where DutyCycle is in percent, between 0 and 100%
-
-	25% duty cycle: 	pulse_length = ((8399 + 1) * 25) / 100 - 1 = 2099
-	50% duty cycle: 	pulse_length = ((8399 + 1) * 50) / 100 - 1 = 4199
-	75% duty cycle: 	pulse_length = ((8399 + 1) * 75) / 100 - 1 = 6299
-	100% duty cycle:	pulse_length = ((8399 + 1) * 100) / 100 - 1 = 8399
-
-	Remember: if pulse_length is larger than TIM_Period, you will have output HIGH all the time
-*/
-
-	TIM_OCStruct.TIM_Pulse = pulse_length; /* 25% duty cycle */
-	TIM_OC1Init(TIM4, &TIM_OCStruct);
-	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-}
 
 int main(void)
 {
 	//estructura con todas la I/O de la placa (diria que mas...)
 	GPIO_InitTypeDef gpio;//variable para usar la estructura
-    TIM_TimeBaseInitTypeDef timerInitStructure;
-    TIM_TimeBaseInitTypeDef timerInitStructure_led;
-	int i = 0, rand_tled = 0, max_input_value = 0x7FFFFFFF;
-
+    int rand_tled = 0;
+	int aux_timer;
 	int dutyCycle = 0;
-	int j = 0;
+	int arand_tled = 0;
+
+	tm4_periode = 0;
+
+
 
 	initLed3(gpio);
 	initButton(gpio);
 
-
+	//Inicializamos el Random
 	TM_RNG_Init();
-	/*
-	 * Generate random, wee use the ddelay function (1 = 3ms) to wait until the random value
-	 * */
 
+	//Generamos valores random y mapeamos a los valores que cosnideramos válidos
+	//para poder hacer un delay de entre 1s y 5s
 	while(rand_tled<=334 || rand_tled >=1670){
-		j=TM_RNG_Get();
-		if(j<0 )j*=-1;
-		rand_tled = map(j,334,1670);
+		arand_tled = TM_RNG_Get();
+		if(arand_tled < 0 )arand_tled *= -1;
+		rand_tled = map(arand_tled,334,1670);
 	}
 	GPIO_ResetBits(GPIOG, GPIO_Pin_13);
 	delay(rand_tled);
 	GPIO_SetBits(GPIOG, GPIO_Pin_13);
-	GPIO_SetBits(GPIOG, GPIO_Pin_1);
 
-	//InitializeTimer(timerInitStructure);
 	TIM2_INT_Init();
+
+	while(!GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)) {
+
+	}
+	timer_value= timer_value/100; //convertimos a ms
+
+	tm4_periode = (timer_value * 288)/100;//escalamos el periodo necesario para el pwm en ms
+
+	GPIO_ResetBits(GPIOG, GPIO_Pin_13);
+	GPIO_ResetBits(GPIOG, GPIO_Pin_5);
+
+	dutyCycle = (double)(tm4_periode*50.0)/(timer_value);
+
+	//Rutinas de inicializacion del PWM
+	TM_LEDS_PWM_Init();
+
+	//Configuramos la interrupcion externa
+	Configure_PD0();
+	//Inicializamos la interrupcion para el PWM
+	TIM4_INT_Init(tm4_periode);
+	TM_PWM_Init(dutyCycle);
+
 	while (1)
 	{
-		//GPIO_SetBits(GPIOG, GPIO_Pin_1);
-		/*if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET){
-			TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-			//GPIO_ToggleBits(GPIOG, GPIO_Pin_1);
-			//timerValue+=50;
-			//timerValue+=128;
-			timerValue++;
-		}*/
-
-		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)) {
-			GPIO_ResetBits(GPIOG, GPIO_Pin_13);
-			//dutyCycle = 5000/timerValue;
-
-			TM_LEDS_PWM_Init();
-			TM_TIMER_PWM_Init(timerValue);
-			TM_PWM_Init(timerValue);
-		}
+		GPIO_ResetBits(GPIOG, GPIO_Pin_5);
 	}
 }
